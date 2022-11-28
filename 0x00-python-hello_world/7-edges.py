0cd707ae04bfb0395616fffaf8c522b7d1bea70c
Sue-wesh <shiroweru007@gmail.com>
2022-11-28 12:48:46 -0800

    quiz7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[1:8]}")
+print("First 3 letters: {}"
+        .format(word[:3]))
+print("Last 2 letters: {}"
+        .format(word[-2:]))
+print("Middle word: {}"
+        .format(word[1:8]))
