a4daf1b8031cb2104f126271a4ac9ac8919c6922
Sue-wesh <shiroweru007@gmail.com>
2022-11-29 13:10:39 -0800

    quiz2

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num_in_str = repr(number)
+last_str = num_in_str[-1]
+last_digit = int(last_str)
+if last_digit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+elif last_digit == 0:
+    print("Last digit of {} is {} and is 0".format(number, last_digit))
+elif last_digit < 6:
+    print("Last digit of {} is {} and is less than 6 and not 0");
