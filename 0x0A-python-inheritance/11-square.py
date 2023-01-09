6774c5a7ad7991c916a6ba86fd96044199cabd02
Sue-wesh <shiroweru007@gmail.com>
2023-01-09 12:17:37 -0800

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,6 +9,6 @@ class Square(Rectangle):
     def __init__(self, size):
         """instates with private attr size"""
         
-        self.integer_validator
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
