30fd06bf94b0bc460bb13b46361c63404b543803
Sue-wesh <shiroweru007@gmail.com>
2023-01-18 02:58:19 -0800

    rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+"""defines a class Rectangle that inherits from Base"""
+
+from models.base import Base
+
+class Rectangle(Base):
+    """represents a rectange."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize width, height, x to 0 and y to 0."""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        self.__y = value
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        """prints in stdout the Rectangle instance"""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+
+        for h in range(self.height):
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+    def __str__(self):
+        """__str__ method implementation"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
