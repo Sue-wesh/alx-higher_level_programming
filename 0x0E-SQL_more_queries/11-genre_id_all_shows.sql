e4b90a26c10f187fa476fafde55b84cb0a47d655
Sue-wesh <shiroweru007@gmail.com>
2023-02-16 11:28:59 -0800

    Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
--- Import the database dump of hbtn_0d_tvshows to your MySQL server
+-- lists all shows contained in the database hbtn_0d_tvshows
 SELECT t.title AS title, g.genre_id AS genre_id FROM tv_shows t LEFT JOIN tv_show_genres g ON t.id = g.show_id ORDER BY t.title, g.genre_id;
