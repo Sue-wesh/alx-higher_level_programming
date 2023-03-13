0c745cd9e3f14621e7fb91e242023f7202030d23
Sue-wesh <shiroweru007@gmail.com>
2023-03-13 12:44:12 -0700

    create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'undefined is undefined' : process.argv[3] === 'undefined' ? process.argv[2] + ' is undefined ' :  process.argv[2] + ' is ' + process.argv[3])
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
