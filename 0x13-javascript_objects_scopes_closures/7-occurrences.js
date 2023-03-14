c89b0c5f0ad6dca6aed3c8510dfeeb10937fa648
Sue-wesh <shiroweru007@gmail.com>
2023-03-14 08:49:00 -0700

    number of occurrences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  const count = 0;
-  const num = 0;
-  if (list[num] === searchElement) count++;
-  else { exports.nbOccurences = function (list[num++])}
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
