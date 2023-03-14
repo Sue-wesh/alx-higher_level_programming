8b0aeb1e4c2ab7d7f2954fc1245d08c35c689409
Sue-wesh <shiroweru007@gmail.com>
2023-03-14 08:02:02 -0700

    number of ooccurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  const count = 0;
+  const num = 0;
+  if (list[num] === searchElement) count++;
+  else { exports.nbOccurences = function (list[num++])}
+};
