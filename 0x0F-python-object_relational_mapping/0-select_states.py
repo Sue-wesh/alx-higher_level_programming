f342399da2a52fd8151e75dc8bdc094d4024349b
Sue-wesh <shiroweru007@gmail.com>
2023-03-23 22:21:08 -0700

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 list all states from the database hbtn_0e_0_usa
 take 3 arquments ie mysql username, mysql password and database name
 """
@@ -7,7 +7,7 @@ from sys import argv
 from MySQLdb import connect
 
 if __name__ == "__main__":
-    db = connect(user=argv[1], passwd=argv[2], 
+    db = connect(user=argv[1], passwd=argv[2],
                  db=argv[3], host="localhost", port=3306)
     query = db.cursor()
     query.execute("SELECT * FROM  states ORDER BY states.id")
