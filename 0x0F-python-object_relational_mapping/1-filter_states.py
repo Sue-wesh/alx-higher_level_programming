1645656929a798495a9400963d8a4f625fd40d16
Sue-wesh <shiroweru007@gmail.com>
2023-03-23 22:30:15 -0700

    states starting with letter N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
                  db=argv[3], host="localhost", port=3306)
     c = db.cursor()
     c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-    [print(state) for state in c.fetchall()]
+    for state in c.fetchall():
+        print(state)
     c.close()
     db.close()
