b1491f01586aa9d83ec4b3afa1f4007f17a0e27b
Sue-wesh <shiroweru007@gmail.com>
2023-03-23 20:40:19 -0700

    states with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,8 +5,10 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
-                         database=sys.argv[3], host="localhost", port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], host="localhost", port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    c.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id""")
+    [print(state) for state in c.fetchall()]
+    c.close()
+    db.close()
