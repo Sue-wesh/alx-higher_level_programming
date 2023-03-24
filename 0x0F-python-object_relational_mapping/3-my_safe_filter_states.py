5be508b13cb8bf8495a8a92e9f195b5c4cc27b17
Sue-wesh <shiroweru007@gmail.com>
2023-03-23 23:29:57 -0700

    safe filter states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-# display all values in the states table where name 
-# matches the argument and is safe from MySQL injections
-# takes 4 arguments <mysql username> \
-#                   <mysql password> \
-#                   <database name> \
-#                   <state name searched>
-import sys
-import MySQLdb
+"""
+display all values in the states table where name 
+matches the argument and is safe from MySQL injections
+takes 4 arguments <mysql username> \
+                  <mysql password> \
+                  <database name> \
+                  <state name searched>
+"""
+from sys import argv
+from MySQLdb import connect
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = connect(user=argv[1], passwd=argv[2], 
+                 db=argv[3], host="localhost", port=3306)
     query = db.cursor()
-    state = sys.argv[4]
-    search = "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC".format(state)
+    state = argv[4]
+    search = "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC"
+    search.format(state)
     query.execute(search)
     [print(state) for state in query.fetchall()]
     
