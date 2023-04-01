5c581714ccf5cbc06b4363c5c9393e3b3669db36
Sue-wesh <shiroweru007@gmail.com>
2023-04-01 01:35:34 -0700

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
         content = response.read()
         print("Body response:")
         print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
-                format(type(content), content, content.decode('utf-8')))
+              format(type(content), content, content.decode('utf-8')))
