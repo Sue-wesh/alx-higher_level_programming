cf1cb2cb21d869472956d52270a6b44045a58372
Sue-wesh <shiroweru007@gmail.com>
2023-04-01 01:35:51 -0700

    send a request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" takes in a URL, sends a request to the URL and displays the value of 
-the X-Request-Id variable found in the header of the response"""
+""" takes in a URL, sends a request to the URL and displays the value of
+the X-Request-Id variable found in the header of the response
+"""
 import urllib.request
 import sys
+
 
 if __name__ == "__main__":
     request = urllib.request.Request(sys.argv[1])
