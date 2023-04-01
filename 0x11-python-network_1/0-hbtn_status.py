93886ab083a054a1304d8b2c11d907c80df276da
Sue-wesh <shiroweru007@gmail.com>
2023-04-01 01:19:33 -0700

    urllib.request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
+                format(type(content), content, content.decode('utf-8')))
