ab864a82e200dfcdf385735d7547ad52f10935ae
Susan <shi4git@gmail.com>
2023-04-21 12:35:53 -0700

    relationship between tables

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,13 +10,13 @@ import sys
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                      .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
     for st in session.query(State).order_by(State.id):
-        print(state.id, state.name, sep=": ")
-        for c in state.cities:
+        print(st.id, st.name, sep=": ")
+        for c in st.cities:
             print("    {}: {}".format(c.id, c.name))
         session.close()
