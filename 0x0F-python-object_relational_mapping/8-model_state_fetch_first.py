ba1a01a5767a48d3a04d0db319ca0035b2e45e7c
Susan <shi4git@gmail.com>
2023-04-21 11:24:15 -0700

    first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+prints the first State object from the database and use sqlalchemy
+"""
 from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
