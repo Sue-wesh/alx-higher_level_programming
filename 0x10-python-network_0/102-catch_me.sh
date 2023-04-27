c011c813168d8ebc759ced8af2102a5e0a3ccb8b
Susan <shi4git@gmail.com>
2023-04-27 12:14:50 -0700

    server to respind with a message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # server to respond witha message
-curl -sL -X PUT "0.0.0.0:5000/catch_me" -H "Origin: You got me!" -d "user_id=98"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -H "Origin: You got me!" -d "user_id=98"
