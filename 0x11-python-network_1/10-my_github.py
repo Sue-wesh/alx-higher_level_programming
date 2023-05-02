174250aa463fcece480043b07d19b04ed5e12f7d
Susan <shi4git@gmail.com>
2023-05-01 23:25:38 -0700

    use github api to get id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials (username and password) 
+and uses the GitHub API to display your id
+"""
+
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import sys
+
+    basic = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+
+    res = requests.get('https://api.github.com/user', auth=basic)
+    res = res.json()
+    print(res.get('id'))
