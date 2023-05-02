2d6c680f3d00c51249bccea27c85a1154492c641
Susan <shi4git@gmail.com>
2023-05-01 23:49:05 -0700

    holberton challenge

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Please list 10 commits (from the most recent to oldest) of the repository “rails” 
+by the user “rails”
+You must use the GitHub API, here is the documentation 
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    
+    res = requests.get(url)
+    res = res.json()
+    try:
+        for i in range(0, 10):
+            print(f"{res[i].get('sha')}: {res[i]['commit']['author']['name']}")
+    except IndexError:
+        pass
